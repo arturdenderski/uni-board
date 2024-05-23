cd26a3b0c3ea9fda0c6ed059e0284037c5fbbe05
Mateusz Kubiak <matkubiak888@gmail.com>
2024-05-23 03:54:19 +0100

    removing post

--- src/components/EditPostPopup.js
+++ src/components/EditPostPopup.js
@@ -5,6 +5,7 @@ import {
   DialogContent,
   Button,
   TextField,
+  Stack,
 } from '@mui/material';
 
 function EditPostPopup({ id, post, open, onClose }) {
@@ -53,7 +54,7 @@ function EditPostPopup({ id, post, open, onClose }) {
           price,
           // You might need to update photo data differently if it's stored differently
           // For simplicity, this example assumes it's stored as a URL
-          photo: photo || post.photo, // Keep the existing photo if no new one is uploaded
+          photo, // Keep the existing photo if no new one is uploaded
         };
       }
       return other;
@@ -64,6 +65,10 @@ function EditPostPopup({ id, post, open, onClose }) {
 
     // Close the popup
     onClose();
+  };
+
+  const handleRemovePhoto = () => {
+    setPhoto('');
   };
 
   const handleImageChange = (event) => {
@@ -77,46 +82,106 @@ function EditPostPopup({ id, post, open, onClose }) {
     }
   };
 
+  const handleDelete = () => {
+    const userPosts = JSON.parse(localStorage.getItem('myposts')) || [];
+    const updatedPosts = userPosts.filter((other) => {
+      return other.id !== post.id;
+    });
+    localStorage.setItem('myposts', JSON.stringify(updatedPosts));
+    onClose();
+  };
+
+  const handleCancel = () => {
+    onClose();
+    if (post) {
+      setTitle(post.title);
+      setDescription(post.description);
+      setLocation(post.location);
+      setPrice(post.price);
+      setPhoto(post.photo);
+    }
+  };
+
+  const handleImgError = (event) => {
+    event.target.src = './no-image.jpg';
+  };
+
   return (
     <Dialog open={open} onClose={onClose}>
       <DialogTitle>Edit Post</DialogTitle>
       <DialogContent>
-        <TextField
-          label="Title"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          fullWidth
-          margin="normal"
-        />
-        <TextField
-          label="Description"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-          fullWidth
-          margin="normal"
-          multiline
-          rows={4}
-        />
-        <TextField
-          label="Location"
-          value={location}
-          onChange={(e) => setLocation(e.target.value)}
-          fullWidth
-          margin="normal"
-        />
-        <TextField
-          label="Price"
-          value={price}
-          onChange={(e) => setPrice(e.target.value)}
-          fullWidth
-          margin="normal"
-        />
-        <input
-          type="file"
-          accept="image/*"
-          onChange={handleImageChange}
+        <Stack direction="row">
+          <Stack direction="column">
+            <img
+              alt="post photo"
+              src={photo}
+              onError={handleImgError}
+              style={{ width: '300px', height: '300px' }}
+            />
+            <input
+              type="file"
+              accept="image/*"
+              onChange={handleImageChange}
+              style={{ marginTop: '16px' }}
+            />
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleRemovePhoto}
+              style={{ marginTop: '16px' }}
+            >
+              Remove Photo
+            </Button>
+          </Stack>
+          <Stack direction="column">
+            <TextField
+              label="Title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              fullWidth
+              margin="normal"
+            />
+            <TextField
+              label="Description"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              fullWidth
+              margin="normal"
+              multiline
+              rows={4}
+            />
+            <TextField
+              label="Location"
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
+              fullWidth
+              margin="normal"
+            />
+            <TextField
+              label="Price"
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+              fullWidth
+              margin="normal"
+            />
+          </Stack>
+        </Stack>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={handleCancel}
           style={{ marginTop: '16px' }}
-        />
+        >
+          Cancel
+        </Button>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={handleDelete}
+          style={{ marginTop: '16px' }}
+        >
+          Delete Post
+        </Button>
         <Button
           variant="contained"
           color="primary"
