eb5da9ce46f016d2368640dd28e2259b815a1c0d
Mateusz Kubiak <matkubiak888@gmail.com>
2024-05-23 01:27:42 +0100

    better image input

--- src/components/SettingsPopup.js
+++ src/components/SettingsPopup.js
@@ -6,12 +6,31 @@ import {
   DialogContent,
   Button,
   Typography,
+  DialogActions,
+  Grid,
+  styled,
+  IconButton,
 } from '@mui/material';
+
+const VisuallyHiddenInput = styled('input')({
+  clip: 'rect(0 0 0 0)',
+  clipPath: 'inset(50%)',
+  height: 1,
+  overflow: 'hidden',
+  position: 'absolute',
+  bottom: 0,
+  left: 0,
+  whiteSpace: 'nowrap',
+  width: 1,
+});
 
 function SettingsPopup({ open, onClose }) {
   const [name, setName] = useState('');
   const [surname, setSurname] = useState('');
   const [email, setEmail] = useState('');
+  const [profilePic, setProfilePic] = useState(
+    localStorage.getItem('userProfilePicture')
+  );
 
   useEffect(() => {
     // Retrieve user details from localStorage
@@ -25,7 +44,16 @@ function SettingsPopup({ open, onClose }) {
     setEmail(storedEmail);
   }, [open]);
 
+  useEffect(() => {
+    setProfilePic(localStorage.getItem('userProfilePicture'));
+  }, [open]);
+
+  const handleCancel = () => {
+    onClose();
+  };
+
   const handleSave = () => {
+    localStorage.setItem('userProfilePicture', profilePic);
     onClose();
   };
 
@@ -34,60 +62,100 @@ function SettingsPopup({ open, onClose }) {
     if (file) {
       const reader = new FileReader();
       reader.onload = () => {
-        localStorage.setItem('userProfilePicture', reader.result);
+        setProfilePic(reader.result);
       };
       reader.readAsDataURL(file);
     }
   };
 
+  const handleProfilePicClick = () => {
+    document.getElementById('profile-pic-selector').click();
+  };
+
   return (
-    <Dialog open={open} onClose={onClose}>
-      <DialogTitle>Settings</DialogTitle>
-      <DialogContent>
-        <img
-          src="profilePicture1.png"
-          alt="Profile"
-          style={{
-            width: '50px',
-            height: '50px',
-            borderRadius: '50%',
-            marginRight: '16px',
-          }}
-        />
-        <Typography variant="subtitle1" gutterBottom>
-          Name:
-        </Typography>
-        <Typography variant="body1" gutterBottom>
-          {name}
-        </Typography>
+    <Dialog fullWidth={false} open={open} onClose={onClose}>
+      <DialogTitle style={{ fontSize: '30px' }}>Settings</DialogTitle>
+      <DialogContent dividers>
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+          <Grid item xs={12}>
+            <IconButton onClick={handleProfilePicClick}>
+              <img
+                src={profilePic}
+                alt="Profile picture"
+                style={{
+                  width: '300px',
+                  height: '300px',
+                  borderRadius: '20px',
+                  margin: 'auto',
+                }}
+              />
+            </IconButton>
+          </Grid>
 
-        <Typography variant="subtitle1" gutterBottom>
-          Surname:
-        </Typography>
-        <Typography variant="body1" gutterBottom>
-          {surname}
-        </Typography>
+          <Grid item xs={6}>
+            <Typography variant="subtitle1" gutterBottom>
+              Name:
+            </Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <Typography variant="body1" gutterBottom>
+              {name}
+            </Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <Typography variant="subtitle1" gutterBottom>
+              Surname:
+            </Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <Typography variant="body1" gutterBottom>
+              {surname}
+            </Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <Typography variant="subtitle1" gutterBottom>
+              Email:
+            </Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <Typography variant="body1" gutterBottom>
+              {email}
+            </Typography>
+          </Grid>
+        </Grid>
 
-        <Typography variant="subtitle1" gutterBottom>
-          Email:
-        </Typography>
-        <Typography variant="body1" gutterBottom>
-          {email}
-        </Typography>
-        <input
-          type="file"
-          accept="image/*"
-          onChange={handleImageChange}
-          style={{ marginTop: '16px' }}
-        />
         <Button
+          component="label"
+          role={undefined}
           variant="contained"
-          color="primary"
-          onClick={handleSave}
-          style={{ marginTop: '16px' }}
+          tabIndex={-1}
         >
-          Save
+          Upload new picture
+          <VisuallyHiddenInput
+            id="profile-pic-selector"
+            onChange={handleImageChange}
+            type="file"
+            accept="image/*"
+          />
         </Button>
+        <DialogActions dividers>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleCancel}
+            style={{ marginTop: '16px' }}
+          >
+            Cancel
+          </Button>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleSave}
+            style={{ marginTop: '16px' }}
+          >
+            Save
+          </Button>
+        </DialogActions>
       </DialogContent>
     </Dialog>
   );
